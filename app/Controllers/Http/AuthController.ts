a10edb5c421e4ab217e44e021ea0d08ab17f0ad1
Vinicius Ribeiro <vayne99@gmail.com>
2022-11-08 16:04:45 -0300

    feat: auth validation

--- app/Controllers/Http/AuthController.ts
+++ app/Controllers/Http/AuthController.ts
@@ -2,19 +2,25 @@ import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 import Env from '@ioc:Adonis/Core/Env'
 import User from 'App/Models/User';
 import { sendRecoverPasswordMail } from 'App/Services/sendMail';
+import LoginValidator from 'App/Validators/Auth/LoginValidator';
 
 const EXPIRES_IN = Env.get('NODE_ENV') === 'development' ? '' : '30mins'
 
 export default class AuthController {
     public async login({ auth, request, response }: HttpContextContract) {
         try {
+            await request.validate(LoginValidator);
             const { email, password } = request.only(['email', 'password']);
             const user = await User.query().where('email', email).preload('roles').first();
             const token = await auth.use('api').attempt(email, password,
                 { name: user?.name, expiresIn: EXPIRES_IN });
             return response.ok({ user: user, token: token });
         } catch (error) {
-            return response.forbidden({ message: "Invalid credentials", error: error });
+            if (error.messages) {
+                response.badRequest(error.messages);
+                return;
+            }
+            return response.forbidden({ message: "Authentication failed", originalMessage: error.message });
         }
     }
 
