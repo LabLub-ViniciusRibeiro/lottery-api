c7e37e277fb7745401229e05f4ee858d35c2efe7
Vinicius Ribeiro <vayne99@gmail.com>
2023-05-14 15:46:13 +0100

    fix: changeresponse formats to match frontend

--- app/Controllers/Http/UsersController.ts
+++ app/Controllers/Http/UsersController.ts
@@ -1,29 +1,28 @@
-import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
-import Database from '@ioc:Adonis/Lucid/Database';
-import WelcomeEmail from 'App/Mailers/WelcomeEmail';
-import Role from 'App/Models/Role';
-import User from 'App/Models/User'
-import StoreValidator from 'App/Validators/User/StoreValidator';
-import UpdateValidator from 'App/Validators/User/UpdateValidator';
+import type { HttpContextContract } from "@ioc:Adonis/Core/HttpContext";
+import Database from "@ioc:Adonis/Lucid/Database";
+import WelcomeEmail from "App/Mailers/WelcomeEmail";
+import Role from "App/Models/Role";
+import User from "App/Models/User";
+import StoreValidator from "App/Validators/User/StoreValidator";
+import UpdateValidator from "App/Validators/User/UpdateValidator";
 
 export default class UsersController {
   public async index({ bouncer, request, response }: HttpContextContract) {
-    await bouncer.authorize('is adm');
+    await bouncer.authorize("is adm");
     const { page, perPage, noPaginate } = request.qs();
     try {
       if (noPaginate) {
-        const users = await User
-          .query()
-          .preload('roles', roles => roles.select('name'))
-        return response.ok(users)
+        const users = await User.query().preload("roles", (roles) =>
+          roles.select("name")
+        );
+        return response.ok(users);
       }
-      const users = await User
-        .query()
-        .preload('roles', roles => roles.select('name'))
+      const users = await User.query()
+        .preload("roles", (roles) => roles.select("name"))
         .paginate(page || 1, perPage || 10);
-      return response.ok(users)
+      return response.ok(users);
     } catch (error) {
-      return response.notFound({ error })
+      return response.notFound({ error });
     }
   }
 
@@ -42,59 +41,72 @@ export default class UsersController {
     }
 
     try {
-      const player = await Role.findBy('name', 'player', trx)
+      const player = await Role.findBy("name", "player", trx);
       if (player === null) throw new Error();
-      if (player) newUser.related('roles').attach([player.id])
+      if (player) newUser.related("roles").attach([player.id]);
     } catch (error) {
       await trx.rollback();
       return response.badRequest(error);
     }
     let user: User | null;
     try {
-      user = await User.query().where('email', newUser.email).preload('roles').firstOrFail();
+      user = await User.query()
+        .where("email", newUser.email)
+        .preload("roles")
+        .firstOrFail();
       response.created(user);
     } catch (error) {
-      return response.badRequest({ message: 'error creating user', originalError: error.message });
+      return response.badRequest({
+        message: "error creating user",
+        originalError: error.message,
+      });
     }
 
     try {
       const welcomeEmail = new WelcomeEmail(user);
-      await welcomeEmail.send()
+      await welcomeEmail.send();
       trx.commit();
     } catch (error) {
       trx.rollback();
-      return response.badRequest({ message: 'Error sending welcome email', original: error.message });
+      return response.badRequest({
+        message: "Error sending welcome email",
+        original: error.message,
+      });
     }
   }
 
   public async show({ auth, params, response }: HttpContextContract) {
+    console.log(auth.user);
     const userSecureId = params.id;
     const userAuthenticated = auth.user;
     const user = auth.user as User;
-    const userRoles = (await user.related('roles').query()).map(role => role.name);
+    const userRoles = (await user.related("roles").query()).map(
+      (role) => role.name
+    );
 
-    if (!userRoles.includes('admin')) {
+    if (!userRoles.includes("admin")) {
       try {
         if (userSecureId !== userAuthenticated?.secureId)
-          throw new Error('You are not authorized to see this user info');
+          throw new Error("You are not authorized to see this user info");
       } catch (error) {
-        return response.forbidden({ message: error.message })
+        return response.forbidden({ message: error.message });
       }
     }
 
     try {
-      const user = await User
-        .query()
-        .where('secure_id', params.id)
-        .preload('bets', (bets) => {
+      const user = await User.query()
+        .where("secure_id", params.id)
+        .preload("bets", (bets) => {
           const today = new Date();
           today.setMonth(today.getMonth() - 1);
-          const formated = today.toLocaleDateString('en-US').replace(/\//g, '-');
-          bets.where('created_at', '>', formated);
+          const formated = today
+            .toLocaleDateString("en-US")
+            .replace(/\//g, "-");
+          bets.where("created_at", ">", formated);
         })
         .first();
       if (!user) {
-        throw new Error('User not found')
+        throw new Error("User not found");
       }
       response.ok(user);
     } catch (error) {
@@ -102,44 +114,55 @@ export default class UsersController {
     }
   }
 
-  public async update({ auth, request, params, response }: HttpContextContract) {
-  await request.validate(UpdateValidator);
-  const userSecureId = params.id;
-  const userAuthenticated = auth.user;
+  public async update({
+    auth,
+    request,
+    params,
+    response,
+  }: HttpContextContract) {
+    await request.validate(UpdateValidator);
+    const userSecureId = params.id;
+    const userAuthenticated = auth.user;
 
-  try {
-    if (userSecureId !== userAuthenticated?.secureId)
-      throw new Error('You are not authorized to edit this user info');
-  } catch (error) {
-    return response.forbidden({ message: error.message })
+    try {
+      if (userSecureId !== userAuthenticated?.secureId)
+        throw new Error("You are not authorized to edit this user info");
+    } catch (error) {
+      return response.forbidden({ message: error.message });
+    }
+
+    const requestBody = request.only(["name", "email", "password"]);
+
+    let updatedUser: User;
+
+    try {
+      updatedUser = await User.findByOrFail("secure_id", userSecureId);
+      await updatedUser.merge(requestBody).save();
+    } catch (error) {
+      return response.badRequest(error);
+    }
+
+    try {
+      const user = await User.query()
+        .where("email", updatedUser.email)
+        .preload("roles")
+        .first();
+      return response.ok(user);
+    } catch (error) {
+      return response.badRequest(error);
+    }
   }
-
-  const requestBody = request.only(["name", "email", "password"]);
-
-  let updatedUser: User;
-
-  try {
-    updatedUser = await User.findByOrFail('secure_id', userSecureId);
-    await updatedUser.merge(requestBody).save();
-  } catch (error) {
-    return response.badRequest(error);
-  }
-
-  try {
-    const user = await User.query().where('email', updatedUser.email).preload('roles').first();
-    return response.ok(user)
-  } catch (error) {
-    return response.badRequest(error)
-  }
-}
 
   public async destroy({ params, response }: HttpContextContract) {
-  try {
-    const user = await User.findByOrFail('secure_id', params.id);
-    await user.delete();
-    response.ok({ message: 'User deleted!' });
-  } catch (error) {
-    response.ok({ message: 'Error deleting user', originalMessage: error.message });
+    try {
+      const user = await User.findByOrFail("secure_id", params.id);
+      await user.delete();
+      response.ok({ message: "User deleted!" });
+    } catch (error) {
+      response.ok({
+        message: "Error deleting user",
+        originalMessage: error.message,
+      });
+    }
   }
-}
 }
--- app/Models/Game.ts
+++ app/Models/Game.ts
@@ -1,37 +1,36 @@
-import { DateTime } from 'luxon'
-import { BaseModel, column } from '@ioc:Adonis/Lucid/Orm'
-import { compose } from '@ioc:Adonis/Core/Helpers'
-import { Filterable } from '@ioc:Adonis/Addons/LucidFilter'
-import GameFilter from './Filters/GameFilter'
+import { DateTime } from "luxon";
+import { BaseModel, column } from "@ioc:Adonis/Lucid/Orm";
+import { compose } from "@ioc:Adonis/Core/Helpers";
+import { Filterable } from "@ioc:Adonis/Addons/LucidFilter";
+import GameFilter from "./Filters/GameFilter";
 
 export default class Game extends compose(BaseModel, Filterable) {
-
-  public static $filter = () => GameFilter
+  public static $filter = () => GameFilter;
 
   @column({ isPrimary: true })
-  public id: number
+  public id: number;
 
   @column()
-  public type: string
+  public type: string;
 
   @column()
-  public description: string
+  public description: string;
 
   @column()
-  public range: number
+  public range: number;
 
   @column()
-  public price: number
+  public price: number;
 
   @column()
-  public minMaxNumber: number
+  public minMaxNumber: number;
 
   @column()
-  public color: string
+  public color: string;
 
   @column.dateTime({ autoCreate: true })
-  public createdAt: DateTime
+  public createdAt: DateTime;
 
   @column.dateTime({ autoCreate: true, autoUpdate: true })
-  public updatedAt: DateTime
+  public updatedAt: DateTime;
 }
